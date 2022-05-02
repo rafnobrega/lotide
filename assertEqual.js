a56045aad63696e3d09aa0f8d7d5a33f1bc7592a
Rafael Nobrega <rafael.rlnr@gmail.com>
2022-05-02 15:51:03 -0700

    improved asserEqual function with template strings

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅ Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("🚫 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🚫 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 // TEST CODE
