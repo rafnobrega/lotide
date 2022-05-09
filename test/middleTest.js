9eeb94636593bfac7d040c0f36555127d711607f
Rafael Nobrega <rafael.rlnr@gmail.com>
2022-05-08 22:39:33 -0700

    middleTest.js created

---
+++ test/middleTest.js
@@ -0,0 +1,17 @@
+const assertArraysEqual = require("../assertArraysEqual");
+const middle = require("../middle");
+
+// TEST CODE
+console.log("Log tests ran without assertArraysEqual:")
+console.log(middle([])); // => []
+console.log(middle([1])); // => []
+console.log(middle([1, 2])); // => []
+console.log(middle([1, 2, 3])); // => [2]
+console.log(middle([1, 2, 3, 4, 5])); // => [3]
+console.log(middle([1, 2, 3, 4])); // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+console.log("Tests ran using assertArraysEqual calling middle:")
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
