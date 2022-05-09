3fd989b993f9015671040169ac0b91db5b4c8257
Rafael Nobrega <rafael.rlnr@gmail.com>
2022-05-08 22:22:41 -0700

    eqArraysTest.js created

---
+++ test/eqArraysTest.js
@@ -0,0 +1,11 @@
+const assertEqual = require("../assertEqual");
+const eqArrays = require("../eqArrays");
+
+
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+
+//TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
