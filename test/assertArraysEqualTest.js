eaf089bc0ab6a1a5c0f8ee91926d2ec7488adf8a
Rafael Nobrega <rafael.rlnr@gmail.com>
2022-05-08 22:32:02 -0700

    assertArraysEqualTest.js created

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,9 @@
+const assertArraysEqual = require("../assertArraysEqual");
+
+// TEST CODE
+assertArraysEqual("Bootcamp", "Bootcamp");
+assertArraysEqual("Lighthouse Labs", "Bootcamp");
+assertArraysEqual(1, 1);
+assertArraysEqual(false, false);
+assertArraysEqual(false, true);
+assertArraysEqual(true, true);
